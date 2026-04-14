2f58c8ed6f1f77cadbbde5dd61b210c92f48e66f
Christoph Mühlhans <christoph.muehlhans@tugraz.at>
2026-04-14 11:24:37 +0200

    Add sync read and write

--- turtlebot3/include/turtlebot3/dynamixel_sdk_wrapper.hpp
+++ turtlebot3/include/turtlebot3/dynamixel_sdk_wrapper.hpp
@@ -28,20 +28,20 @@ class DynamixelSDKWrapper
     DynamixelSDKWrapper(const char *port_name = "/dev/ttyACM0");
     ~DynamixelSDKWrapper();
 
-    /**
+    /** ************************************************************************
      * @brief Initializes the motors.
-     * 
+     *
      * @return true if the initialization was successful.
      * @return false otherwise.
-     */
+     * ************************************************************************/
     bool init();
 
-    /**
+    /** ************************************************************************
      * @brief Reads the position encoder value of a motor.
-     * 
+     *
      * @param dxl_id Motor id
      * @return The position encoder value.
-     */
+     * ************************************************************************/
     inline int32_t readPosition(uint8_t dxl_id)
     {
       int32_t data = 0;
@@ -54,15 +54,15 @@ class DynamixelSDKWrapper
       return data;
     }
 
-    /**
+    /** ************************************************************************
      * @brief Sets the PWM value of a motor.
      *        The set value will stay the same until it is changed again.
-     * 
+     *
      * @param dxl_id Motor id
      * @param pwm_value PWM value
      * @return true if the PWM value was set successfully.
      * @return false otherwise.
-     */
+     * ************************************************************************/
     inline bool writePWM(uint8_t dxl_id, int16_t pwm_value)
     {
       return write(
@@ -72,9 +72,55 @@ class DynamixelSDKWrapper
       );
     }
 
+    /** ************************************************************************
+     * @brief Simultaneously reads the position encoder values of both motors.
+     *
+     * @param pos_left The left position encoder value
+     * @param pos_right The right position encoder value
+     * @return true if the encoder values were successfully read.
+     * @return false otherwise.
+     * ************************************************************************/
+    inline bool syncReadPosition(int32_t *pos_left, int32_t *pos_right)
+    {
+      return syncRead(
+        dxl_control_table::PRESENT_POSITION,
+        reinterpret_cast<uint32_t *>(pos_left),
+        reinterpret_cast<uint32_t *>(pos_right)
+      );
+    }
+
+    /** ************************************************************************
+     * @brief Simultaneously writes the PWM values of both motors.
+     *
+     * @param pwm_value_left The left PWM value
+     * @param pwm_value_right The right PWM value
+     * @return true if the PWM values were successfully written.
+     * @return false otherwise.
+     * ************************************************************************/
+    inline bool syncWritePWM(int16_t pwm_value_left, int16_t pwm_value_right)
+    {
+      return syncWrite(
+        dxl_control_table::GOAL_PWM,
+        reinterpret_cast<uint8_t *>(&pwm_value_left),
+        reinterpret_cast<uint8_t *>(&pwm_value_right)
+      );
+    }
+
   private:
     bool read(uint8_t dxl_id, const ControlItem &control_item, uint8_t *data);
     bool write(uint8_t dxl_id, const ControlItem &control_item, uint8_t *data);
+
+    bool syncRead(
+      const ControlItem &control_item,
+      uint32_t *data_left,
+      uint32_t *data_right
+    );
+
+    bool syncWrite(
+      const ControlItem &control_item,
+      uint8_t *data_left,
+      uint8_t *data_right
+    );
 };
 
 } /* namespace turtlebot3 */
--- turtlebot3/src/dynamixel_sdk_wrapper.cpp
+++ turtlebot3/src/dynamixel_sdk_wrapper.cpp
@@ -137,4 +137,87 @@ bool DynamixelSDKWrapper::write(
   return true;
 }
 
+// -----------------------------------------------------------------------------
+bool DynamixelSDKWrapper::syncRead(
+  const ControlItem &control_item,
+  uint32_t *data_left,
+  uint32_t *data_right
+)
+{
+  dynamixel::GroupSyncRead sync_read(
+    port_handler_,
+    packet_handler_,
+    control_item.address_,
+    control_item.length_
+  );
+
+  sync_read.addParam(DXL_LEFT_ID);
+  sync_read.addParam(DXL_RIGHT_ID);
+
+  int result = sync_read.txRxPacket();
+
+  if (result != COMM_SUCCESS)
+  {
+    std::cerr << packet_handler_->getTxRxResult(result) << std::endl;
+    return false;
+  }
+
+  if (
+    !sync_read.isAvailable(
+      DXL_LEFT_ID,
+      control_item.address_,
+      control_item.length_
+    ) ||
+
+    !sync_read.isAvailable(
+      DXL_RIGHT_ID,
+      control_item.address_,
+      control_item.length_
+    )
+  )
+    return false;
+
+  *data_left = sync_read.getData(
+    DXL_LEFT_ID,
+    control_item.address_,
+    control_item.length_
+  );
+
+  *data_right = sync_read.getData(
+    DXL_RIGHT_ID,
+    control_item.address_,
+    control_item.length_
+  );
+
+  return true;
+}
+
+// -----------------------------------------------------------------------------
+bool DynamixelSDKWrapper::syncWrite(
+  const ControlItem &control_item,
+  uint8_t *data_left,
+  uint8_t *data_right
+)
+{
+  dynamixel::GroupSyncWrite sync_write(
+    port_handler_,
+    packet_handler_,
+    control_item.address_,
+    control_item.length_
+  );
+
+  sync_write.addParam(DXL_LEFT_ID, data_left);
+  sync_write.addParam(DXL_RIGHT_ID, data_right);
+
+  int result = sync_write.txPacket();
+
+  if (result != COMM_SUCCESS)
+  {
+    std::cout << packet_handler_->getTxRxResult(result) << std::endl;
+    return false;
+  }
+
+  return true;
+}
+
 } /* namespace turtlebot3 */
