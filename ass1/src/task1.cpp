954cb29b110bb07ea43334d85517c7195dfcfc18
Aaron Armbruster <aa13.arm@gmail.com>
2026-04-22 22:23:07 +0200

    test in Stunde

--- ass1/src/task1.cpp
+++ ass1/src/task1.cpp
@@ -1,8 +1,21 @@
 #include "turtlebot3/dynamixel_sdk_wrapper.hpp"
 
+#include <thread>
+
 // -----------------------------------------------------------------------------
 int main()
 {
+  turtlebot3::DynamixelSDKWrapper dxl;
+  if(!dxl.init())
+    return -1;
+
+  dxl.syncWritePWM(300,300);
+  dxl.readPosition(dxl.DXL_LEFT_ID);
+
+  std::this_thread::sleep_for(std::chrono::seconds(5));
+
+  dxl.syncWritePWM(-300,300);
+  std::this_thread::sleep_for(std::chrono::seconds(5));
   // TODO Implement task 1
   return 0;
 }
