0e8e8ed8f8f7c82e051e0c554684f81fb42c82f4
Christoph Mühlhans <christoph.muehlhans@tugraz.at>
2026-05-26 13:29:55 +0200

    Add assignment 3

---
+++ ass3/src/task1.cpp
@@ -0,0 +1,9 @@
+#include "turtlebot3/dynamixel_sdk_wrapper.hpp"
+#include "turtlebot3/lds03_wrapper.hpp"
+
+// -----------------------------------------------------------------------------
+int main()
+{
+  // TODO Implement taks 1
+  return 0;
+}
